75d03861bb244d451fec2aea4f4be8322d9c5b7a
Alana303 <alanaesmeralda045@gmail.com>
2025-07-26 20:22:00 +0300

    Fix incorrect Navbar and Footer import paths

--- src/components/Newsfeed.jsx
+++ src/components/Newsfeed.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './Navbar';
+import Footer from './Footer';
 import './styles.css';
 
 export default function Newsfeed() {
